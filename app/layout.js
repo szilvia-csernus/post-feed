4fda7d27e4a6b8eb6ce872f30b17a3aab278187f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-08-29 18:11:20 +0100

    add metadata comment for layout page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/header';
 import './globals.css';
 
+// This metadata will be merged with the metadata from the page
 export const metadata = {
   title: 'NextPosts',
   description: 'Browse and share amazing posts.',
