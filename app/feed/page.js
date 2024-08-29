1a67d1e8fdfd675a90c003cdf3a7eb96d1d7ccc5
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-08-29 18:09:49 +0100

    add dynamic metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,15 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// generateMetadata is a reserved function name that can be used to generate metadata for the page
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all our ${numberOfPosts} posts`,
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
