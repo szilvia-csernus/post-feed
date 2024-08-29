ed4c586af26235509e87f2819014a4ae3c684dba
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-08-29 18:01:12 +0100

    add static metadata

--- app/page.js
+++ app/page.js
@@ -3,6 +3,28 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export const metadata = {
+  title: 'Latest Posts',
+  description: 'Browse our latest posts.',
+  // The openGraph property controls the appearance of your page when shared on social media.
+  // openGraph: {
+  //   title: 'PostFeed',
+  //   description: 'Browse our latest posts.',
+  //   url: 'https://yourwebsite.com/latest-posts',
+  //   siteName: 'PostFeed',
+  //   images: [
+  //     {
+  //       url: 'https://yourwebsite.com/logo.jpg',
+  //       width: 1200,
+  //       height: 630,
+  //       alt: 'Logo',
+  //     },
+  //   ],
+  //   type: 'website',
+  //   locale: 'en_US',
+  // },
+}
+
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
   return <Posts posts={latestPosts} />;
